54344db7945e2cb5a19f8118969ac1f07b36e0fe
Marcus Costa <53195917+Marcus-V-Freitas@users.noreply.github.com>
2025-10-12 15:37:07 -0300

    Adicionado arquivo ignorado pelo .gitignore.

---
+++ MVFC.SQLCraft/Servicos/Logs/IDatabaseLogger.cs
@@ -0,0 +1,7 @@
+﻿namespace MVFC.SQLCraft.Servicos.Logs;
+
+public interface IDatabaseLogger {
+    Task OnBeforeExecuteAsync(string sql, object? bindings, CancellationToken ct = default);
+    Task OnAfterExecuteAsync(string sql, object? bindings, TimeSpan elapsed, CancellationToken ct = default);
+    Task OnErrorAsync(string sql, object? bindings, Exception ex, CancellationToken ct = default);
+}
--- MVFC.SQLCraft/Usings.cs
+++ MVFC.SQLCraft/Usings.cs
@@ -3,6 +3,5 @@ global using SqlKata;
 global using SqlKata.Compilers;
 global using SqlKata.Execution;
 global using System.Data;
-global using MVFC.SQLCraft.Servicos;
 global using MVFC.SQLCraft.Servicos.Factory;
 global using MVFC.SQLCraft.Servicos.Logs;
